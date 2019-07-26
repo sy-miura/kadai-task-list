6c5942280bcbc63c3945995371423d3c79141f09
sy-miura <three.reverse@gmail.com>
2019-07-27 07:52:07 +0900

    Option#get causes an error.So Option#get changes to match method

--- app/controllers/TaskDetailController.scala
+++ app/controllers/TaskDetailController.scala
@@ -11,8 +11,11 @@ class TaskDetailController @Inject()(components: ControllerComponents)
   extends AbstractController (components) with I18nSupport {
 
   def index(taskId:Long):Action[AnyContent] = Action {implicit request =>
-    val task = Task.findById(taskId).get
-    Ok(views.html.show(task))
+    Task.findById(taskId) match{
+      case Some(task) => Ok(views.html.show(task))
+      case None => BadRequest("該当するタスクが見つかりません")
+    }
+
   }
 
 }
